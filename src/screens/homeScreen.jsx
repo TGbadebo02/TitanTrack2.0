8c0a3a865309c0a1fb2643dc97aed352eb2abdc3
TGbadebo02 <tj.gbadebo02@gmail.com>
2025-04-13 00:11:01 +0100

    creating the homeScreen and report screen.

---
+++ src/screens/homeScreen.jsx
@@ -0,0 +1,11 @@
+import {View, Text, StyleSheet, Image, ImageBackground, TouchableOpacity,} from 'react-native';
+import React from 'react';
+
+
+const homeScreen = () => {
+    return(
+        <View> </View>
+    );
+};
+
+export default homeScreen;
---
+++ src/screens/reportScreen.jsx
@@ -0,0 +1,11 @@
+import {View, Text, StyleSheet, Image, ImageBackground, TouchableOpacity,} from 'react-native';
+import React from 'react';
+
+
+const reportScreen = () => {
+    return(
+        <View> </View>
+    );
+};
+
+export default reportScreen;
