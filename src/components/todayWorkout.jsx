5bd2c91960c848394857acd5b368359f9d0cbb70
TGbadebo02 <tj.gbadebo02@gmail.com>
2025-08-03 23:45:36 +0100

    Homepage in progress.

---
+++ src/components/todayWorkout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const todayWorkout () => {
+
+};
+
+export default todayWorkout;
+
+const styles = Stylesheet.create({
+
+});
--- src/screens/homeScreen.jsx
+++ src/screens/homeScreen.jsx
@@ -1,11 +1,19 @@
 import {View, Text, StyleSheet, Image, ImageBackground, TouchableOpacity,} from 'react-native';
 import React from 'react';
+import { ScrollView } from 'react-native-gesture-handler';
 
 
 const homeScreen = () => {
     return(
-        <View> </View>
+       <ScrollView>
+
+       </ScrollView>
+
     );
 };
 
 export default homeScreen;
+
+const styles = StyleSheet.create({
+    
+})
