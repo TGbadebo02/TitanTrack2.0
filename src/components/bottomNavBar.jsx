92c48e912cf52b5efa9c418fd8e7bce170ec3bdc
TGbadebo02 <tj.gbadebo02@gmail.com>
2025-08-26 23:51:09 +0100

    HomeScreen - start working on the home screen I will revisit the bottom navbar.

--- src/components/bottomNavBar.jsx
+++ src/components/bottomNavBar.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity} from "react-native";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-//import { Ionicons } from '@expo/vector-icons';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import { Ionicons } from '@expo/vector-icons';
+//import Ionicons from 'react-native-vector-icons/Ionicons';
 import { NavigationContainer } from "@react-navigation/native";
 import profileScreen from "../screens/profileScreen";
 import homeScreen from "../screens/homeScreen";
@@ -11,11 +11,13 @@ import reportScreen from "../screens/reportScreen";
 const Tab = createBottomTabNavigator();
 
 
-const bottomNavbar = () => {
-<NavigationContainer>
+export default function bottomNavbar() {
+  return(
     <Tab.Navigator  
         screenOptions={({ route }) => ({
           headerShown: false,
+          tabBarActiveTintColor: '#4ade80',
+          tabBarInactiveTintColor: 'gray',
           tabBarIcon: ({ color, size, focused }) => {
             const icons = {
               Home: focused ? 'home' : 'home-outline',
@@ -29,24 +31,19 @@ const bottomNavbar = () => {
             // } else if (route.name === 'Profile') {
             //   iconName = 'person-outline';
             // }
-
-            return <Ionicons name={iconName} size={size} color={color} />;
+            //const iconName = ICONS[route.name] ?? 'ellipse';
+            return <Ionicons name="home" size={48} color="tomato" />;
           },
-          tabBarActiveTintColor: '#4ade80',
-          tabBarInactiveTintColor: 'gray',
-          headerShown: false,
         })}
       >
       <Tab.Screen name ="Home" component = {homeScreen}/>
       <Tab.Screen name ="Profile" component = {profileScreen}/>
       <Tab.Screen name = "Report" component ={reportScreen}/>
     </Tab.Navigator>
-</NavigationContainer>
-
+  );
 }
 
 
-export default bottomNavbar;
 
 const styles = StyleSheet.create({
 
